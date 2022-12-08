10e8c585343e47016e74cfcf85da9f29e5a3a0c0
Lucas Martins <Lucascamilo02@gmail.com>
2022-12-08 16:46:41 -0300

    Basic settings change

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%2k+^vg!x$5m3(u3$43z1jq=dj6%wk%8u@wb&#isgrz5=azhx*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'BRT'
 
 USE_I18N = True
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
