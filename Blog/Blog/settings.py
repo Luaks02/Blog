2f950092e6a2e963f618abe7a5027801aa19ccc5
Lucas Martins <Lucascamilo02@gmail.com>
2022-12-11 16:23:05 -0300

    Hiding the secret Key

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%2k+^vg!x$5m3(u3$43z1jq=dj6%wk%8u@wb&#isgrz5=azhx*'
+SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
+ALLOWED_HOSTS = [str(os.getenv("PROJECT_DOMAIN")) + ".glitch.me"]
 
 # Application definition
 
