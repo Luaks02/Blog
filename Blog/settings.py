4a251c9344855f02abdd903d7ac852b2262b6283
Lucas Martins <Lucascamilo02@gmail.com>
2022-12-12 15:31:50 -0300

    Too many folders, manage.py migrate working now

--- Blog/settings.py
+++ Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import mysql.connector
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,9 @@ SECRET_KEY = os.getenv("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [str(os.getenv("PROJECT_DOMAIN")) + ".glitch.me"]
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+#ALLOWED_HOSTS = [str(os.getenv("PROJECT_DOMAIN")) + ".glitch.me", "127.0.0.1"]
 
 # Application definition
 
@@ -79,7 +82,7 @@ DATABASES = {
         'NAME': "teste",
         'USER': 'root',
         'PASSWORD': "batata",
-        'HOST': "localhost",
+        'HOST': "localhost"
     }
 }
 
